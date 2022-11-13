b6e5db6a87d58b99aa3950c0275417ad5c0082fd
milosgostovic92 <milosgostovic@vegait.rs>
2022-11-13 22:09:55 +0100

    Initial starter push

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Gulp-starter
+Setting up Gulp in a project
+
+---------------------------------------------------------
+
+Run node.js command prompt
+
+---------------------------------------------------------
+
+Install gulp globally - "npm install gulp -g"
+
+---------------------------------------------------------
+
+To collect all gulp dependencies run command:
+
+npm install
+
+---------------------------------------------------------
+
+To compile styles/svgs run "gulp" command
+
+---------------------------------------------------------
+
+                         NOTE
+
+If package.json is empty or not updating, run "npm init" command.
+
+---------------------------------------------------------
+
+Some dependencied you might need:
+
+gulp-sass ---->  npm i gulp-sass
+
+gulp-postcss ----> npm i gulp-postcss
+
+gulp-styleliny ----> npm i gulp-stylelint
+
+browsersync ----> npm i browsersync
+
+--------------------------------------------------------
+
+Having error on gulp saas:
+
+1st step:
+Uninstall node-sass:
+npm: npm uninstall node-sass
+
+2nd step:
+Uninstall sass-loader:
+npm: npm uninstall sass-loader
+
+3rd step:
+npm: npm install sass -dev
+
+--------------------------------------------------------- UPDATES ---------------------------------------------------------
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require("gulp-postcss");
+const gulpStylelint = require("gulp-stylelint");
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync").create();
+
+// scss compiler
+gulp.task("scss", () => {
+  return gulp
+    .src("src/scss/styles.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer("last 2 versions")]))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+
+//style lint
+gulp.task("scss-lint", () => {
+  return gulp.src("src/scss/*.scss").pipe(
+    gulpStylelint({
+      reporters: [
+        {
+          formatter: "string",
+          console: true,
+        },
+      ],
+    })
+  );
+});
+
+// copy js files to dist
+gulp.task("js", () => {
+  return gulp.src("src/js/*.js").pipe(gulp.dest("dist/js"));
+});
+
+// copy html files to dist
+gulp.task("html", () => {
+  return gulp.src("src/*.{html,ico}").pipe(gulp.dest("dist"));
+});
+
+// copy assets files to dist
+gulp.task("assets", () => {
+  return gulp.src("src/assets/*/*").pipe(gulp.dest("dist/assets"));
+});
+
+// copy font files to dist
+gulp.task("fonts", () => {
+  return gulp
+    .src("src/fonts/*.{ttf,woff,woff2,eof}")
+    .pipe(gulp.dest("dist/fonts"));
+});
+
+// browser sync
+gulp.task("watch", () => {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "./dist",
+    },
+  });
+  gulp
+    .watch("src/**/*.scss")
+    .on("change", gulp.series("scss", browserSync.reload));
+  gulp
+    .watch("src/*.html")
+    .on("change", gulp.series("copy-html", browserSync.reload));
+  gulp
+    .watch("src/**/*.js")
+    .on("change", gulp.series("copy-js", browserSync.reload));
+});
+
+//build project
+gulp.task(
+  "build",
+  gulp.series("fonts", "scss", "js", "html", "assets")
+);
+
+//to run watch task type: gulp
+gulp.task("default", gulp.series("watch"));
